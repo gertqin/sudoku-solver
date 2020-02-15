bddba90fa818180aba73b3a130c0340fca0c8750
Gert <gertqin@gmail.com>
2020-02-15 19:37:22 +0100

    unsigned & bitToNum optimizations

--- solver.cpp
+++ solver.cpp
@@ -10,131 +10,135 @@ using namespace std;
 #define SIZE 81
 #define ABS(a, b) ((a) > (b) ? (a) - (b) : (b) - (a))
 
-const short ALL_BITS = (1 << 9) - 1;
+const unsigned short ALL_BITS = (1 << 9) - 1;
 
-bool oneBits[ALL_BITS + 1];
+unsigned char bitToNum[ALL_BITS + 1];
 
-char updateCells1[SIZE][20]; // row: 8, col: 8, square remain: 4
-char puzzle[SIZE];
-char solution[SIZE];
-short possibilities[SIZE];
-char missing = 0;
+unsigned char updateCells[SIZE][20]; // row: 8, col: 8, square remain: 4
+unsigned char puzzle[SIZE];
+unsigned char solution[SIZE];
+unsigned short possibilities[SIZE];
+unsigned char missing = 0;
 
 #ifdef DEBUG
+double timeSetup = 0;
 double timeReadInput = 0;
 double timeSetNumber = 0;
 double timeSolve = 0;
+double timeGuess = 0;
 #endif
 
-inline short removeBit(short val)
+inline unsigned short removeBit(unsigned short val)
 {
   return val & (val - 1);
 }
-inline bool isTwoBits(short val)
+inline unsigned char isTwoBits(unsigned short val)
 {
-  return val && oneBits[removeBit(val)];
+  return val && bitToNum[removeBit(val)];
 }
-inline short numToBit(char num)
+inline unsigned short numToBit(unsigned char num)
 {
   return 1 << (num - 1);
 }
-char bitToNum(short bit)
-{
-  char num = 1;
-  while (bit >>= 1)
-  {
-    num++;
-  }
-  return num;
-}
 
 void setup()
-{
-  oneBits[0] = false;
-  for (short i = 1; i <= ALL_BITS; ++i)
-    oneBits[i] = !removeBit(i);
-
-  char rows[9][9];
-  char cols[9][9];
-  char squares[9][9];
-
-  for (int i = 0; i < 9; ++i)
-    for (int j = 0; j < 9; ++j)
-    {
-      rows[i][j] = i * 9 + j;
-      cols[i][j] = j * 9 + i;
-    }
-
-  for (int i = 0; i < 9; ++i)
-  {
-    char row = i / 3 * 3;
-    char col = i % 3 * 3;
-
-    for (int j = 0; j < 3; ++j)
-      for (int k = 0; k < 3; ++k)
-        squares[i][j * 3 + k] = row * 9 + col + j * 9 + k;
-  }
-
-  for (int i = 0; i < SIZE; ++i)
-  {
-    char row = i / 9;
-    char col = i % 9;
-    char square = row / 3 * 3 + col / 3;
-
-    int idx = 0;
-    for (int j = 0; j < 9; ++j)
-      if (rows[row][j] != i)
-        updateCells1[i][idx++] = rows[row][j];
-
-    for (int j = 0; j < 9; ++j)
-      if (cols[col][j] != i)
-        updateCells1[i][idx++] = cols[col][j];
-
-    for (int j = 0; j < 9; ++j)
-    {
-      char squareVal = squares[square][j];
-      if (squareVal % 9 != i % 9 && ABS(squareVal, i) > 2)
-        updateCells1[i][idx++] = squareVal;
-    }
-  }
-}
-
-inline char setNumber(int i)
 {
 #ifdef DEBUG
   clock_t start = clock();
 #endif
 
-  puzzle[i] = bitToNum(possibilities[i]) + '0';
-
-  for (int j = 0; j < 20; ++j)
+  bitToNum[0] = 0;
+  for (unsigned short i = 1; i <= ALL_BITS; ++i)
   {
-    possibilities[updateCells1[i][j]] &= ~possibilities[i];
+    if (removeBit(i))
+    {
+      bitToNum[i] = 0;
+    }
+    else
+    { // single bit number
+      unsigned short bit = i;
+      unsigned char num = 1;
+      while (bit >>= 1)
+      {
+        num++;
+      }
+      bitToNum[i] = num + '0';
+    }
   }
 
-  possibilities[i] = 0;
-  missing--;
+  unsigned char rows[9][9];
+  unsigned char cols[9][9];
+  unsigned char squares[9][9];
+
+  for (unsigned char i = 0; i < 9; ++i)
+    for (unsigned char j = 0; j < 9; ++j)
+    {
+      rows[i][j] = i * 9 + j;
+      cols[i][j] = j * 9 + i;
+    }
+
+  for (unsigned char i = 0; i < 9; ++i)
+  {
+    unsigned char row = i / 3 * 3;
+    unsigned char col = i % 3 * 3;
+
+    for (unsigned char j = 0; j < 3; ++j)
+      for (unsigned char k = 0; k < 3; ++k)
+        squares[i][j * 3 + k] = row * 9 + col + j * 9 + k;
+  }
+
+  for (unsigned char i = 0; i < SIZE; ++i)
+  {
+    unsigned char row = i / 9;
+    unsigned char col = i % 9;
+    unsigned char square = row / 3 * 3 + col / 3;
+
+    unsigned char idx = 0;
+    for (unsigned char j = 0; j < 9; ++j)
+      if (rows[row][j] != i)
+        updateCells[i][idx++] = rows[row][j];
+
+    for (unsigned char j = 0; j < 9; ++j)
+      if (cols[col][j] != i)
+        updateCells[i][idx++] = cols[col][j];
+
+    for (unsigned char j = 0; j < 9; ++j)
+    {
+      unsigned char squareVal = squares[square][j];
+      if (squareVal % 9 != i % 9 && ABS(squareVal, i) > 2)
+        updateCells[i][idx++] = squareVal;
+    }
+  }
 
 #ifdef DEBUG
-  timeSetNumber += ((double)(clock() - start) / CLOCKS_PER_SEC);
+  timeSetup += ((double)(clock() - start) / CLOCKS_PER_SEC);
 #endif
 }
 
+inline unsigned char setNumber(unsigned char num)
+{
+}
+
 void runAlgoFromGuess();
+void runAlgo();
 
 void guess()
 {
-  short possibClone[SIZE];
-  char puzzleClone[SIZE];
-  char missingClone = missing;
+  for (unsigned char i = 0; i < SIZE; ++i)
+    if (!possibilities[i] && !puzzle[i])
+      return;
 
-  for (int i = 0; i < SIZE; ++i)
+  unsigned short possibClone[SIZE];
+  unsigned char puzzleClone[SIZE];
+  unsigned char missingClone = missing;
+
+  for (unsigned char i = 0; i < SIZE; ++i)
   {
     possibClone[i] = possibilities[i];
     puzzleClone[i] = puzzle[i];
   }
 
-  char guessCell = 0;
+  unsigned char guessCell = 0;
   for (; guessCell < SIZE; guessCell++)
   {
     if (isTwoBits(possibilities[guessCell]))
@@ -143,72 +147,72 @@ void guess()
   if (guessCell == SIZE)
     return;
 
-  short bit1 = 1;
+  unsigned short bit1 = 1;
   while (!(bit1 & possibilities[guessCell]))
     bit1 <<= 1;
 
-  short bit2 = possibilities[guessCell] & (possibilities[guessCell] - 1);
+  unsigned short bit2 = possibilities[guessCell] & (possibilities[guessCell] - 1);
 
   possibilities[guessCell] = bit1;
-  runAlgoFromGuess();
+  runAlgo();
 
   if (missing > 0)
   {
     missing = missingClone;
-    for (int i = 0; i < SIZE; ++i)
+    for (unsigned char i = 0; i < SIZE; ++i)
     {
       possibilities[i] = possibClone[i];
       puzzle[i] = puzzleClone[i];
     }
     possibilities[guessCell] = bit2;
-    runAlgoFromGuess();
+    runAlgo();
   }
-}
-
-void runAlgoFromGuess()
-{
-  char progress = 1;
-  while (progress == 1 && missing)
-  {
-    progress = 0;
-    for (int i = 0; i < SIZE; ++i)
-    {
-      if (!possibilities[i] && !puzzle[i])
-      {
-        progress = -1;
-        break;
-      }
-      else if (oneBits[possibilities[i]])
-      {
-        setNumber(i);
-        progress = 1;
-      }
-    }
-  }
-
-  if (missing && !progress)
-    guess();
 }
 
 void runAlgo()
 {
-
-  char progress = 1;
+  unsigned char progress = 1;
   while (progress == 1 && missing)
   {
     progress = 0;
-    for (int i = 0; i < SIZE; ++i)
+    for (unsigned char i = 0; i < SIZE; ++i)
     {
-      if (oneBits[possibilities[i]])
+      if (bitToNum[possibilities[i]])
       {
-        setNumber(i);
+#ifdef DEBUG
+        clock_t start = clock();
+#endif
+
+        puzzle[i] = bitToNum[possibilities[i]];
+
+        for (unsigned char j = 0; j < 20; ++j)
+        {
+          possibilities[updateCells[i][j]] &= ~possibilities[i];
+        }
+
+        possibilities[i] = 0;
+        missing--;
+
+#ifdef DEBUG
+        timeSetNumber += ((double)(clock() - start) / CLOCKS_PER_SEC);
+#endif
         progress = 1;
       }
     }
   }
 
   if (missing && !progress)
+  {
+#ifdef DEBUG
+    clock_t start = clock();
+#endif
+
     guess();
+
+#ifdef DEBUG
+    timeGuess += ((double)(clock() - start) / CLOCKS_PER_SEC);
+#endif
+  }
 }
 
 void solve()
@@ -217,12 +221,11 @@ void solve()
   clock_t start = clock();
 #endif
 
-  for (int i = 0; i < SIZE; ++i)
+  for (unsigned char i = 0; i < SIZE; ++i)
     possibilities[i] = puzzle[i] == '0' ? ALL_BITS : numToBit(puzzle[i] - '0');
 
   missing = SIZE;
 
-  bool progress = true;
   runAlgo();
 
 #ifdef DEBUG
@@ -230,13 +233,13 @@ void solve()
 #endif
 }
 
-bool checkSolution()
+unsigned char checkSolution()
 {
-  for (int i = 0; i < SIZE; ++i)
+  for (unsigned char i = 0; i < SIZE; ++i)
     if (puzzle[i] != solution[i])
-      return false;
+      return 0;
 
-  return true;
+  return 1;
 }
 
 int main()
@@ -245,15 +248,14 @@ int main()
 
   setup();
 
-  FILE *fp;
-  fp = fopen("sudoku.csv", "r");
+  FILE *fp = fopen("sudoku.csv", "r");
 
-  char header[50];
+  unsigned char header[50];
   fscanf(fp, "%[^\n]", header);
 
-  int failed = 0;
+  unsigned int failed = 0;
 
-  for (int i = 0; i < 1000000; ++i)
+  for (unsigned int i = 0; i < 1000000; ++i)
   {
 #ifdef DEBUG
     clock_t sInput = clock();
@@ -276,9 +278,11 @@ int main()
   cout << "took: " << ((double)(end - start) / CLOCKS_PER_SEC) << "s" << endl;
 
 #ifdef DEBUG
+  cout << "time setup: " << timeSetup << endl;
   cout << "time read input: " << timeReadInput << endl;
   cout << "time solve: " << timeSolve << endl;
   cout << "time update number: " << timeSetNumber << endl;
+  cout << "time guess: " << timeGuess << endl;
 #endif
 
   fclose(fp);
