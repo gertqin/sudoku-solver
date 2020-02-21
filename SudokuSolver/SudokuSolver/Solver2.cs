720f0e5e1002f3bcdc1c646f098a9428566b7b98
Gert <gertqin@gmail.com>
2020-02-21 21:26:21 +0100

    solver2

---
+++ SudokuSolver/SudokuSolver/Solver2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SudokuSolver
+{
+  class Solver2
+  {
+
+    public static void Solve()
+    {
+      using (var fs = File.OpenRead("../../../../../sudoku.csv"))
+      {
+        byte[] data = new byte[2];
+        fs.Read(data, 0, 2);
+
+      }
+    }
+  }
+}
