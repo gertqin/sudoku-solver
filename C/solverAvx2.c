3e700cbaea438040df75025abd043f95b622036e
Gert Qin Hansen <GQH@cBrain.net>
2020-03-05 20:02:49 +0100

    wip c

--- C/solverAvx2.c
+++ C/solverAvx2.c
@@ -4,6 +4,7 @@
 #include "time.h"
 
 #define TEST
+#define CHECK_SOLUTION
 
 #define SUDOKU_COUNT 1000000
 
@@ -17,18 +18,18 @@
 #define BYTES_FOR_7_SUDOKUS 1148
 #define BYTES_FOR_8_SUDOKUS 1312
 
-void vecTest() {
-  unsigned long long nums[8];
-  for (int i = 0; i < 8; i++)
-    nums[i] = i + 1;
+#define ROW_OFFSET 1296  // 81 << 4
+#define BOX_OFFSET 1440  // + 9 << 4
+#define COL_OFFSET 1584  // + 9 << 4
+#define DATA_LENGTH 1782 // + 9 << 4
 
-  __m256i v = _mm256_loadu_si256((const __m256i_u *)nums);
-  printf("%lld %lld %lld %lld", v[0], v[1], v[2], v[3]);
-}
-
-double time_ms(clock_t start, clock_t end) {
-  return ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
-}
+// #region function defines
+static void run(const uint8_t *bytes);
+static void solve16sudokus(const uint8_t *sudokus, uint16_t *data);
+static void transformSudokus(const uint8_t *sudokus, uint16_t *data);
+static void setupStep(uint16_t *data, int *r2b);
+static double time_ms(const clock_t start, const clock_t end);
+// #endregion
 
 int main() {
   clock_t start = clock();
@@ -42,10 +43,91 @@ int main() {
   fclose(fp);
 
   clock_t end = clock();
-  printf("Read input took: %fms\n", time_ms(start, end));
+  printf("Reading input took: %.0fms\n", time_ms(start, end));
 
-  for (int i = 0; i < BYTES_FOR_1_SUDOKU; i++)
-    printf("%c", bytes[i]);
+  start = clock();
+  run(bytes);
+  end = clock();
+  printf("Solving 1.000.000 sudokus took: %.0fms\n", time_ms(start, end));
 
   return 0;
+}
+
+static void run(const uint8_t *bytes) {
+  static uint16_t data[DATA_LENGTH];
+
+  for (int i = 0; i < SUDOKU_COUNT; i += 16) {
+    solve16sudokus(&bytes[i * BYTES_FOR_1_SUDOKU], data);
+  }
+}
+
+static void solve16sudokus(const uint8_t *sudokus, uint16_t *data) {
+  transformSudokus(sudokus, data);
+
+  static int r2b[9] = {0, 0, 0, 3, 3, 3, 6, 6, 6};
+
+  setupStep(data, r2b);
+}
+
+static inline void transformSudokus(const uint8_t *sudokus, uint16_t *data) {
+  int i, j, sOffset;
+
+  for (i = 0; i < 16; i++) {
+    sOffset = i * BYTES_FOR_1_SUDOKU;
+    for (j = 0; j < SUDOKU_CELL_COUNT; j++) {
+      data[(j << 4) + i] =
+          (uint16_t)(0b100000000 >> ('9' - sudokus[sOffset + j]));
+    }
+  }
+}
+
+static inline void setupStep(uint16_t *data, int *r2b) {
+  uint16_t *p_rows = &data[ROW_OFFSET], *p_boxs = &data[BOX_OFFSET],
+           *p_cols = &data[COL_OFFSET];
+  int i, rowMaxI = 9 << 4;
+
+  __m256i_u maskVec = _mm256_set1_epi16((short)0b111111111);
+  for (i = 0; i < rowMaxI; i += 16) {
+    _mm256_storeu_si256((__m256i_u *)(p_rows + i), maskVec);
+    _mm256_storeu_si256((__m256i_u *)(p_boxs + i), maskVec);
+    _mm256_storeu_si256((__m256i_u *)(p_cols + i), maskVec);
+  }
+
+  int p = 0, r = 0, b, c, maxB, maxC;
+  __m256i_u *p_r, *p_b, *p_c;
+  for (; r < 9; r++) {
+    p_r = (__m256i_u *)&p_rows[r << 4];
+    __m256i_u rVec = _mm256_loadu_si256(p_r);
+
+    b = r2b[r];
+    c = 0;
+    maxB = b + 3;
+    for (; b < maxB; b++) {
+      p_b = (__m256i_u *)&p_boxs[b << 4];
+      __m256i_u bVec = _mm256_loadu_si256(p_b);
+
+      maxC = c + 3;
+      for (; c < maxC; c++, p++) {
+        p_c = (__m256i_u *)&p_cols[c << 4];
+        __m256i cVec = _mm256_loadu_si256(p_c);
+        __m256i pVec = _mm256_loadu_si256((__m256i_u *)&data[p << 4]);
+
+        rVec = _mm256_andnot_si256(pVec, rVec);
+        bVec = _mm256_andnot_si256(pVec, bVec);
+        cVec = _mm256_andnot_si256(pVec, cVec);
+        _mm256_storeu_si256(p_c, cVec);
+      }
+
+      _mm256_storeu_si256(p_b, bVec);
+    }
+    _mm256_storeu_si256(p_r, rVec);
+  }
+}
+
+static void solveFullIteration() {
+  // todo
+}
+
+static double time_ms(const clock_t start, const clock_t end) {
+  return ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
 }
---
+++ C/solverTest.c
@@ -0,0 +1,183 @@
+
+#include "immintrin.h"
+#include "stdint.h"
+#include "stdio.h"
+#include "time.h"
+
+#define TEST
+
+#define SUDOKU_COUNT 1000000
+
+#define ROW_OFFSET 81
+#define BOX_OFFSET 90
+#define COL_OFFSET 99
+#define DATA_LENGTH 108
+
+static double time_ms(clock_t start, clock_t end) {
+  return ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
+}
+
+static void vecTest() {
+  unsigned long long nums[8];
+  for (int i = 0; i < 8; i++)
+    nums[i] = i + 1;
+
+  __m256i v = _mm256_loadu_si256((const __m256i_u *)nums);
+  printf("%lld %lld %lld %lld", v[0], v[1], v[2], v[3]);
+}
+
+static void loop1(uint32_t *data) {
+  uint32_t *p_puzzle = data, *p_row = data + ROW_OFFSET,
+           *p_box = data + BOX_OFFSET, *p_col = data + COL_OFFSET;
+
+  int r, b, c, p, maxB, maxCol;
+  static int r2b[9] = {0, 0, 0, 3, 3, 3, 6, 6, 6};
+
+  for (int i = 0; i < 10000000; i++) {
+    for (r = 0, p = 0; r < 9; r++) {
+      b = r2b[r];
+      c = 0;
+      maxB = b + 3;
+      for (; b < maxB; b++) {
+
+        maxCol = c + 3;
+        for (; c < maxCol; c++, p++) {
+          p_puzzle[p] = p_row[r] + p_box[b] + p_col[c];
+        }
+      }
+    }
+  }
+}
+
+static void loop2(uint32_t *data) {
+  uint32_t *p_puzzle = data, *p_row = data + ROW_OFFSET,
+           *p_box = data + BOX_OFFSET, *p_col = data + COL_OFFSET;
+
+  int r, b, c, p, maxB, maxCol;
+
+  for (int i = 0; i < 10000000; i++) {
+    for (p = 0; p < 81; p++) {
+      r = p / 9;
+      c = p % 9;
+      b = r / 3 * 3 + c / 3 * 3;
+
+      p_puzzle[p] = p_row[r] + p_box[b] + p_col[c];
+    }
+  }
+}
+
+static void loop3(uint32_t *data) {
+  uint32_t *p_puzzle = data, *p_row = data + ROW_OFFSET,
+           *p_box = data + BOX_OFFSET, *p_col = data + COL_OFFSET;
+
+  int r, b, c, p, maxB, maxCol;
+  static int p2r[81], p2b[81], p2c[81];
+
+  for (p = 0; p < 81; p++) {
+    p2r[p] = p / 9;
+    p2c[p] = p % 9;
+    p2b[p] = p2r[p] / 3 * 3 + p2c[p] / 3 * 3;
+  }
+
+  for (int i = 0; i < 10000000; i++) {
+    for (p = 0; p < 81; p++) {
+      r = p2r[p];
+      c = p2c[p];
+      b = p2b[p];
+
+      p_puzzle[p] = p_row[r] + p_box[b] + p_col[c];
+    }
+  }
+}
+
+static void loop4(uint32_t *data) {
+  uint32_t *p_puzzle = data, *p_row = data + ROW_OFFSET,
+           *p_box = data + BOX_OFFSET, *p_col = data + COL_OFFSET;
+
+  int r, b, c, p, z, maxB, maxCol;
+  static int x[81];
+
+  for (p = 0; p < 81; p++) {
+    r = p / 9;
+    c = p % 9;
+    b = r / 3 * 3 + c / 3 * 3;
+    x[p] = r | (b << 8) | (c << 16);
+  }
+
+  for (int i = 0; i < 10000000; i++) {
+    for (p = 0; p < 81; p++) {
+      z = x[p];
+      r = z & 0x0F;
+      b = (z >> 8) & 0x0F;
+      c = (z >> 16);
+
+      p_puzzle[p] = p_row[r] + p_box[b] + p_col[c];
+    }
+  }
+}
+
+typedef struct {
+  uint32_t *p_r;
+  uint32_t *p_b;
+  uint32_t *p_c;
+} rbc_t;
+
+static void loop5(uint32_t *data) {
+  uint32_t *p_puzzle = data, *p_row = data + ROW_OFFSET,
+           *p_box = data + BOX_OFFSET, *p_col = data + COL_OFFSET;
+
+  int r, b, c, p, z, maxB, maxCol;
+  static rbc_t x[81];
+
+  for (p = 0; p < 81; p++) {
+    r = p / 9;
+    c = p % 9;
+    b = r / 3 * 3 + c / 3 * 3;
+    x[p] = (rbc_t){&p_row[r], &p_box[b], &p_col[c]};
+  }
+
+  for (int i = 0; i < 10000000; i++) {
+    for (p = 0; p < 81; p++) {
+      rbc_t y = x[p];
+
+      p_puzzle[p] = *y.p_r + *y.p_b + *y.p_c;
+    }
+  }
+}
+
+static void loopPerformanceTests() {
+  uint32_t data[DATA_LENGTH];
+
+  for (int i = 0; i < DATA_LENGTH; i++)
+    data[i] = 0;
+
+  clock_t start = clock();
+  loop1(data);
+  clock_t end = clock();
+  printf("Loop1 took: %fms\n", time_ms(start, end));
+
+  start = clock();
+  loop2(data);
+  end = clock();
+  printf("Loop2 took: %fms\n", time_ms(start, end));
+
+  start = clock();
+  loop3(data);
+  end = clock();
+  printf("Loop3 took: %fms\n", time_ms(start, end));
+
+  start = clock();
+  loop4(data);
+  end = clock();
+  printf("Loop4 took: %fms\n", time_ms(start, end));
+
+  start = clock();
+  loop5(data);
+  end = clock();
+  printf("Loop5 took: %fms\n", time_ms(start, end));
+}
+
+int main() {
+  loopPerformanceTests();
+  // todo
+}
