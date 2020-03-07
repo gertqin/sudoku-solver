46c1311c82fbfe1e7af591467a3d033a43e09676
Gert <gertqin@gmail.com>
2020-03-07 21:54:27 +0100

    WIP transform

--- C/solverAvx2.c
+++ C/solverAvx2.c
@@ -3,7 +3,7 @@
 #include "stdio.h"
 #include "time.h"
 
-// #define TEST
+#define TEST
 #define CHECK_SOLUTIONS
 
 #define SUDOKU_COUNT 1000000
@@ -26,14 +26,20 @@
 #pragma region function declerations
 static void run(const uint8_t *bytes);
 static void solve16sudokus(const uint8_t *sudokus, uint16_t *data);
-static void transformSudokus(const uint8_t *sudokus, uint16_t *data);
-static void setupStep(uint16_t *data, int *r2b);
-static void solveFullIteration(uint16_t *data, int *r2b);
-static void solveQueue(uint16_t *data, int *r2b);
+
+static void transform_sudokus(const uint8_t *sudokus, uint16_t *data);
+static void transpose8x16(const uint8_t *p_src, uint16_t *p_dest);
+static void convert2base2(__m256i_u *cellVec, __m256i_u *charVec, __m256i_u *oneVec);
+
+static void setup_step(uint16_t *data, int *r2b);
+static void solve_parallel(uint16_t *data, int *r2b);
+static void solve_queue(uint16_t *data, int *r2b);
+static void solve_cell(__m256i_u *pVec, __m256i_u *rVec, __m256i_u *bVec, __m256i_u *cVec, __m256i_u *zeroVec,
+                       __m256i_u *oneVec);
 static void check_solutions(uint16_t *data, uint16_t *solutions);
 
-static void testTransformSudokus(const uint8_t *sudokus, uint16_t *data);
-static void testSetupStep(uint16_t *data);
+static void test_transform_sudokus(const uint8_t *sudokus, uint16_t *data);
+static void test_setup_step(uint16_t *data);
 
 static double time_ms(const clock_t start, const clock_t end);
 #pragma endregion
@@ -75,42 +81,160 @@ static void run(const uint8_t *bytes) {
 }
 
 static void solve16sudokus(const uint8_t *sudokus, uint16_t *data) {
-  transformSudokus(sudokus, data);
+  transform_sudokus(sudokus, data);
 #ifdef TEST
-  testTransformSudokus(sudokus, data);
+  test_transform_sudokus(sudokus, data);
 #endif
 
   static int r2b[9] = {0, 0, 0, 3, 3, 3, 6, 6, 6};
 
-  setupStep(data, r2b);
+  setup_step(data, r2b);
 #ifdef TEST
-  testSetupStep(data);
+  test_setup_step(data);
 #endif
 
-  for (int i = 0; i < 3; i++) {
-    solveFullIteration(data, r2b);
-  }
-  solveQueue(data, r2b);
+  solve_parallel(data, r2b);
 
 #ifdef CHECK_SOLUTIONS
   static uint16_t solutions[SUDOKU_CELL_COUNT << 4];
-  transformSudokus(&sudokus[SUDOKU_CELL_COUNT + 1], solutions);
+  transform_sudokus(&sudokus[SUDOKU_CELL_COUNT + 1], solutions);
   check_solutions(data, solutions);
 #endif
 }
 
-static inline void transformSudokus(const uint8_t *sudokus, uint16_t *data) {
-  int i, j, sOffset;
+static inline void transform_sudokus(const uint8_t *sudokus, uint16_t *data) {
+  int i, j;
+  const uint8_t *p_src = sudokus;
+  uint16_t *p_dest = data;
+
+  // 5x16 = 80
+  for (i = 0; i < 5; i++) {
+    // solve 16x16
+    transpose8x16(p_src, p_dest);
+    transpose8x16(p_src + (BYTES_FOR_1_SUDOKU << 3), p_dest + 8);
+
+    p_src += 16;
+    p_dest += (1 << 8);
+  }
 
   for (i = 0; i < 16; i++) {
-    sOffset = i * BYTES_FOR_1_SUDOKU;
-    for (j = 0; j < SUDOKU_CELL_COUNT; j++) {
-      data[(j << 4) + i] = (uint16_t)(0b100000000 >> ('9' - sudokus[sOffset + j]));
-    }
+    *p_dest = (uint16_t)(0b100000000 >> ('9' - *p_src));
+    p_src += BYTES_FOR_1_SUDOKU;
+    ++p_dest;
   }
 }
 
-static inline void setupStep(uint16_t *data, int *r2b) {
+// transpose 8 rows x 16 cols, with input in bytes and output in ushorts
+static inline void transpose8x16(const uint8_t *p_src, uint16_t *p_dest) {
+  __m256i_u v1, v2, v3, v4, v5, v6, v7, v8, lo12, lo34, lo56, lo78, hi12, hi34, hi56, hi78;
+
+  v1 = _mm256_cvtepi8_epi16(_mm256_extracti128_si256(_mm256_loadu_si256((__m256i_u *)p_src), 0));
+  v2 = _mm256_cvtepi8_epi16(_mm256_extracti128_si256(_mm256_loadu_si256((__m256i_u *)(p_src + BYTES_FOR_1_SUDOKU)), 0));
+  v3 = _mm256_cvtepi8_epi16(_mm256_extracti128_si256(_mm256_loadu_si256((__m256i_u *)(p_src + BYTES_FOR_2_SUDOKUS)), 0));
+  v4 = _mm256_cvtepi8_epi16(_mm256_extracti128_si256(_mm256_loadu_si256((__m256i_u *)(p_src + BYTES_FOR_3_SUDOKUS)), 0));
+  v5 = _mm256_cvtepi8_epi16(_mm256_extracti128_si256(_mm256_loadu_si256((__m256i_u *)(p_src + BYTES_FOR_4_SUDOKUS)), 0));
+  v6 = _mm256_cvtepi8_epi16(_mm256_extracti128_si256(_mm256_loadu_si256((__m256i_u *)(p_src + BYTES_FOR_5_SUDOKUS)), 0));
+  v7 = _mm256_cvtepi8_epi16(_mm256_extracti128_si256(_mm256_loadu_si256((__m256i_u *)(p_src + BYTES_FOR_6_SUDOKUS)), 0));
+  v8 = _mm256_cvtepi8_epi16(_mm256_extracti128_si256(_mm256_loadu_si256((__m256i_u *)(p_src + BYTES_FOR_7_SUDOKUS)), 0));
+
+  lo12 = _mm256_unpacklo_epi16(v1, v2);
+  lo34 = _mm256_unpacklo_epi16(v3, v4);
+  lo56 = _mm256_unpacklo_epi16(v5, v6);
+  lo78 = _mm256_unpacklo_epi16(v7, v8);
+  hi12 = _mm256_unpackhi_epi16(v1, v2);
+  hi34 = _mm256_unpackhi_epi16(v3, v4);
+  hi56 = _mm256_unpackhi_epi16(v5, v6);
+  hi78 = _mm256_unpackhi_epi16(v7, v8);
+
+  v1 = lo12;
+  v2 = lo34;
+  v3 = lo56;
+  v4 = lo78;
+  v5 = hi12;
+  v6 = hi34;
+  v7 = hi56;
+  v8 = hi78;
+
+  lo12 = _mm256_unpacklo_epi32(v1, v2);
+  lo34 = _mm256_unpacklo_epi32(v3, v4);
+  lo56 = _mm256_unpacklo_epi32(v5, v6);
+  lo78 = _mm256_unpacklo_epi32(v7, v8);
+  hi12 = _mm256_unpackhi_epi32(v1, v2);
+  hi34 = _mm256_unpackhi_epi32(v3, v4);
+  hi56 = _mm256_unpackhi_epi32(v5, v6);
+  hi78 = _mm256_unpackhi_epi32(v7, v8);
+
+  v1 = lo12;
+  v2 = lo34;
+  v3 = hi12;
+  v4 = hi34;
+  v5 = lo56;
+  v6 = lo78;
+  v7 = hi56;
+  v8 = hi78;
+
+  lo12 = _mm256_unpacklo_epi64(v1, v2);
+  lo34 = _mm256_unpacklo_epi64(v3, v4);
+  lo56 = _mm256_unpacklo_epi64(v5, v6);
+  lo78 = _mm256_unpacklo_epi64(v7, v8);
+  hi12 = _mm256_unpackhi_epi64(v1, v2);
+  hi34 = _mm256_unpackhi_epi64(v3, v4);
+  hi56 = _mm256_unpackhi_epi64(v5, v6);
+  hi78 = _mm256_unpackhi_epi64(v7, v8);
+
+  v1 = lo12;
+  v2 = hi12;
+  v3 = lo34;
+  v4 = hi34;
+  v5 = lo56;
+  v6 = hi56;
+  v7 = lo78;
+  v8 = hi78;
+
+  __m256i_u nineCharVec = _mm256_set1_epi16('9');
+  __m256i_u oneVec = _mm256_set1_epi32(0b100000000);
+  convert2base2(&v1, &nineCharVec, &oneVec);
+  convert2base2(&v2, &nineCharVec, &oneVec);
+  convert2base2(&v3, &nineCharVec, &oneVec);
+  convert2base2(&v4, &nineCharVec, &oneVec);
+  convert2base2(&v5, &nineCharVec, &oneVec);
+  convert2base2(&v6, &nineCharVec, &oneVec);
+  convert2base2(&v7, &nineCharVec, &oneVec);
+  convert2base2(&v8, &nineCharVec, &oneVec);
+
+  _mm_storeu_si128((__m128i_u *)p_dest, _mm256_extracti128_si256(v1, 0));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0x10), _mm256_extracti128_si256(v2, 0));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0x20), _mm256_extracti128_si256(v3, 0));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0x30), _mm256_extracti128_si256(v4, 0));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0x40), _mm256_extracti128_si256(v5, 0));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0x50), _mm256_extracti128_si256(v6, 0));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0x60), _mm256_extracti128_si256(v7, 0));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0x70), _mm256_extracti128_si256(v8, 0));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0x80), _mm256_extracti128_si256(v1, 1));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0x90), _mm256_extracti128_si256(v2, 1));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0xA0), _mm256_extracti128_si256(v3, 1));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0xB0), _mm256_extracti128_si256(v4, 1));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0xC0), _mm256_extracti128_si256(v5, 1));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0xD0), _mm256_extracti128_si256(v6, 1));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0xE0), _mm256_extracti128_si256(v7, 1));
+  _mm_storeu_si128((__m128i_u *)(p_dest + 0xF0), _mm256_extracti128_si256(v8, 1));
+}
+
+static inline void convert2base2(__m256i_u *cellVec, __m256i_u *nineCharVec, __m256i_u *nineBitVec) {
+  __m256i_u cellsInBase10 = _mm256_sub_epi16(*nineCharVec, *cellVec);
+  __m256i_u lowCellsInBase2 =
+      _mm256_srlv_epi32(*nineBitVec, _mm256_cvtepu16_epi32(_mm256_extracti128_si256(cellsInBase10, 0)));
+
+  __m256i_u highCellsInBase2 =
+      _mm256_srlv_epi32(*nineBitVec, _mm256_cvtepu16_epi32(_mm256_extracti128_si256(cellsInBase10, 1)));
+
+  __m256i_u cellsInBase2 = _mm256_packus_epi32(lowCellsInBase2, highCellsInBase2);
+  // shuffle packed bytes into order 00 10 01 11 = 1,3,2,4
+  cellsInBase2 = _mm256_permute4x64_epi64(cellsInBase2, 0b11100100);
+  *cellVec = cellsInBase2;
+}
+
+static inline void setup_step(uint16_t *data, int *r2b) {
   uint16_t *p_rows = &data[ROW_OFFSET], *p_boxs = &data[BOX_OFFSET], *p_cols = &data[COL_OFFSET];
   int i, rowMaxI = 9 << 4;
 
@@ -152,130 +276,92 @@ static inline void setupStep(uint16_t *data, int *r2b) {
   }
 }
 
-static inline void solveFullIteration(uint16_t *data, int *r2b) {
-  uint16_t *p_rows = &data[ROW_OFFSET], *p_boxs = &data[BOX_OFFSET], *p_cols = &data[COL_OFFSET];
-
-  int p = 0, r = 0, b, c, maxB, maxC;
-  __m256i_u *p_r, *p_b, *p_c, *p_p;
-  __m256i_u zeroVec = _mm256_setzero_si256();
-  __m256i_u oneVec = _mm256_set1_epi16((short)1);
-
-  for (; r < 9; r++) {
-    p_r = (__m256i_u *)&p_rows[r << 4];
-    __m256i_u rVec = _mm256_loadu_si256(p_r);
-
-    b = r2b[r];
-    c = 0;
-    maxB = b + 3;
-    for (; b < maxB; b++) {
-      p_b = (__m256i_u *)&p_boxs[b << 4];
-      __m256i_u bVec = _mm256_loadu_si256(p_b);
-
-      maxC = c + 3;
-      for (; c < maxC; c++, p++) {
-        p_c = (__m256i_u *)&p_cols[c << 4];
-        p_p = (__m256i_u *)&data[p << 4];
-        __m256i_u cVec = _mm256_loadu_si256(p_c);
-        __m256i_u pVec = _mm256_loadu_si256(p_p);
-
-        __m256i_u bits = _mm256_and_si256(rVec, bVec);
-        bits = _mm256_and_si256(bits, cVec);
-        bits = _mm256_or_si256(bits, pVec);
-
-        __m256i_u mask = _mm256_cmpeq_epi16(_mm256_and_si256(bits, _mm256_sub_epi16(bits, oneVec)), zeroVec);
-
-        bits = _mm256_and_si256(mask, bits);
-
-        pVec = _mm256_or_si256(bits, pVec);
-        rVec = _mm256_andnot_si256(bits, rVec);
-        bVec = _mm256_andnot_si256(bits, bVec);
-        cVec = _mm256_andnot_si256(bits, cVec);
-
-        _mm256_storeu_si256(p_p, pVec);
-        _mm256_storeu_si256(p_c, cVec);
-      }
-
-      _mm256_storeu_si256(p_b, bVec);
-    }
-    _mm256_storeu_si256(p_r, rVec);
-  }
-}
-
 typedef struct {
-  int p, r, b, c;
+  __m256i_u *p_r, *p_b, *p_c, *p_p;
 } cell_t;
 
-static inline void solveQueue(uint16_t *data, int *r2b) {
+static inline void solve_parallel(uint16_t *data, int *r2b) {
   uint16_t *p_rows = &data[ROW_OFFSET], *p_boxs = &data[BOX_OFFSET], *p_cols = &data[COL_OFFSET];
 
-  int qLen = SUDOKU_CELL_COUNT * 3, qEnd = 0;
+  int qLen = SUDOKU_CELL_COUNT << 1, qEnd = 0;
   cell_t queue[qLen];
 
-  int p = 0, r = 0, b, c, maxB, maxC;
+  int i, p, r, b, c, maxB, maxC;
   __m256i_u *p_r, *p_b, *p_c, *p_p;
   __m256i_u zeroVec = _mm256_setzero_si256();
   __m256i_u oneVec = _mm256_set1_epi16((short)1);
 
-  for (; r < 9; r++) {
-    b = r2b[r];
-    c = 0;
-    maxB = b + 3;
-    for (; b < maxB; b++) {
-      maxC = c + 3;
-      for (; c < maxC; c++, p++) {
-        p_p = (__m256i_u *)&data[p << 4];
-        __m256i pVec = _mm256_loadu_si256(p_p);
+  i = 2;
+  do {
+    for (p = 0, r = 0; r < 9; r++) {
+      p_r = (__m256i_u *)&p_rows[r << 4];
+      __m256i_u rVec = _mm256_loadu_si256(p_r);
 
-        if ((uint32_t)_mm256_movemask_epi8(_mm256_cmpeq_epi16(pVec, zeroVec)) > 0) {
-          cell_t cell = (cell_t){p << 4, r << 4, b << 4, c << 4};
-          queue[qEnd++] = cell;
+      for (b = r2b[r], maxB = b + 3, c = 0; b < maxB; b++) {
+        p_b = (__m256i_u *)&p_boxs[b << 4];
+        __m256i_u bVec = _mm256_loadu_si256(p_b);
+
+        for (maxC = c + 3; c < maxC; c++, p++) {
+          p_c = (__m256i_u *)&p_cols[c << 4];
+          p_p = (__m256i_u *)&data[p << 4];
+          __m256i_u cVec = _mm256_loadu_si256(p_c);
+          __m256i_u pVec = _mm256_loadu_si256(p_p);
+
+          solve_cell(&pVec, &rVec, &bVec, &cVec, &zeroVec, &oneVec);
+
+          _mm256_storeu_si256(p_p, pVec);
+          _mm256_storeu_si256(p_c, cVec);
+
+          if (i == 0 && (uint32_t)_mm256_movemask_epi8(_mm256_cmpeq_epi16(pVec, zeroVec)) > 0) {
+            cell_t cell = (cell_t){p_r, p_b, p_c, p_p};
+            queue[qEnd++] = cell;
+          }
         }
+
+        _mm256_storeu_si256(p_b, bVec);
       }
+      _mm256_storeu_si256(p_r, rVec);
     }
-  }
+  } while (i-- != 0);
 
   int qIdx = 0;
-  while (qIdx < qEnd && qIdx < qLen) {
+  while (qIdx < qEnd && qEnd < qLen) {
     cell_t cell = queue[qIdx];
 
-    p_p = (__m256i_u *)&data[cell.p];
-    p_r = (__m256i_u *)&p_rows[cell.r];
-    p_b = (__m256i_u *)&p_boxs[cell.b];
-    p_c = (__m256i_u *)&p_cols[cell.c];
+    __m256i_u rVec = _mm256_loadu_si256(cell.p_r);
+    __m256i_u bVec = _mm256_loadu_si256(cell.p_b);
+    __m256i_u cVec = _mm256_loadu_si256(cell.p_c);
+    __m256i_u pVec = _mm256_loadu_si256(cell.p_p);
 
-    __m256i_u rVec = _mm256_loadu_si256(p_r);
-    __m256i_u bVec = _mm256_loadu_si256(p_b);
-    __m256i_u cVec = _mm256_loadu_si256(p_c);
-    __m256i_u pVec = _mm256_loadu_si256(p_p);
+    solve_cell(&pVec, &rVec, &bVec, &cVec, &zeroVec, &oneVec);
 
-    __m256i_u bits = _mm256_and_si256(rVec, bVec);
-    bits = _mm256_and_si256(bits, cVec);
-    bits = _mm256_or_si256(bits, pVec);
+    _mm256_storeu_si256(cell.p_p, pVec);
+    _mm256_storeu_si256(cell.p_c, cVec);
+    _mm256_storeu_si256(cell.p_b, bVec);
+    _mm256_storeu_si256(cell.p_r, rVec);
 
-    __m256i_u mask = _mm256_cmpeq_epi16(_mm256_and_si256(bits, _mm256_sub_epi16(bits, oneVec)), zeroVec);
-    bits = _mm256_and_si256(mask, bits);
-
-    pVec = _mm256_or_si256(bits, pVec);
-    rVec = _mm256_andnot_si256(bits, rVec);
-    bVec = _mm256_andnot_si256(bits, bVec);
-    cVec = _mm256_andnot_si256(bits, cVec);
-
-    _mm256_storeu_si256(p_p, pVec);
-    _mm256_storeu_si256(p_c, cVec);
-    _mm256_storeu_si256(p_b, bVec);
-    _mm256_storeu_si256(p_r, rVec);
-
-    if (qEnd < qLen && (uint32_t)_mm256_movemask_epi8(_mm256_cmpeq_epi16(pVec, zeroVec)) > 0) {
+    if ((uint32_t)_mm256_movemask_epi8(_mm256_cmpeq_epi16(pVec, zeroVec)) > 0) {
       queue[qEnd++] = cell;
     }
     ++qIdx;
   }
 
-  int qIdx2 = qIdx;
-  int qLen2 = qLen;
-  int qEnd2 = qEnd;
+  queueLengthTotal += qIdx;
+}
 
-  queueLengthTotal += qEnd;
+static inline void solve_cell(__m256i_u *pVec, __m256i_u *rVec, __m256i_u *bVec, __m256i_u *cVec, __m256i_u *zeroVec,
+                              __m256i_u *oneVec) {
+  __m256i_u bits = _mm256_and_si256(*rVec, *bVec);
+  bits = _mm256_and_si256(bits, *cVec);
+  bits = _mm256_or_si256(bits, *pVec);
+
+  __m256i_u mask = _mm256_cmpeq_epi16(_mm256_and_si256(bits, _mm256_sub_epi16(bits, *oneVec)), *zeroVec);
+  bits = _mm256_and_si256(mask, bits);
+
+  *pVec = _mm256_or_si256(bits, *pVec);
+  *rVec = _mm256_andnot_si256(bits, *rVec);
+  *bVec = _mm256_andnot_si256(bits, *bVec);
+  *cVec = _mm256_andnot_si256(bits, *cVec);
 }
 
 static inline void check_solutions(uint16_t *data, uint16_t *solutions) {
@@ -293,7 +379,7 @@ static inline void check_solutions(uint16_t *data, uint16_t *solutions) {
 }
 
 #pragma region tests
-static inline void testTransformSudokus(const uint8_t *sudokus, uint16_t *data) {
+static inline void test_transform_sudokus(const uint8_t *sudokus, uint16_t *data) {
   int i, j, sOffset;
   int testData[SUDOKU_CELL_COUNT << 4];
 
@@ -306,12 +392,12 @@ static inline void testTransformSudokus(const uint8_t *sudokus, uint16_t *data) 
 
   for (i = 0; i < SUDOKU_CELL_COUNT << 4; i++) {
     if (data[i] != testData[i]) {
-      printf("transform fail: %d <> %d", data[i], testData[i]);
+      printf("transform fail: %d <> %d (i=%d)", data[i], testData[i], i);
       exit(1);
     }
   }
 }
-static void testSetupStep(uint16_t *data) {
+static void test_setup_step(uint16_t *data) {
   int len = DATA_LENGTH - ROW_OFFSET;
   uint16_t dataArr[len];
   for (int i = 0; i < len; i++) {
@@ -346,6 +432,4 @@ static void testSetupStep(uint16_t *data) {
 }
 #pragma endregion
 
-static double time_ms(const clock_t start, const clock_t end) {
-  return ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
-}
+static double time_ms(const clock_t start, const clock_t end) { return ((double)(end - start) * 1000 / CLOCKS_PER_SEC); }
